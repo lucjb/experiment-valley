d97c1c3c12573642a03c7bec324ef408aa29bd6f
Lucas Bernardi <lucejb@gmail.com>
2025-10-21 20:55:27 +0200

    adjusted script

--- js/ui-controller.js
+++ js/ui-controller.js
@@ -524,14 +524,14 @@ const UIController = {
 
             // Define challenge sequence for each round
             const challengeSequences = {
-                1: [inconclusive().withOverdue(), twymansLawTrap(), partialLoser()],
+                1: [winner(), inconclusive(), partialLoser()],
                 2: [partialLoser().withVisitorsLoss(), partialLoser().withSampleRatioMismatch(), partialLoser().withBaseRateMismatch()],
                 3: [slowCompletion(), fastWinner(), fastLoserWithPartialWeek()],
                 4: [slowCompletion().withBaseRateMismatch(), fastLoserWithPartialWeek().withSampleRatioMismatch(), loser()],
                 5: [partialWinner(), winner().withLowerIsBetter(), inconclusive()],
-                    6: [twymansLawTrap(), inconclusive(), loser().withLowerIsBetter()],
+                6: [twymansLawTrap(), inconclusive(), fastLoserWithPartialWeek()],
                 7: [partialWinner().withLowerIsBetter(), bigLoser().withLowerIsBetter(), loser().withLowerIsBetter()],
-                8: [partialLoser().withSampleRatioMismatch(), winner(), fastLoserWithPartialWeek()],
+                8: [partialLoser().withSampleRatioMismatch(), winner(), loser().withLowerIsBetter()],
                 9: [inconclusive().withOverdue(), winner(), inconclusive()]
             };
 
